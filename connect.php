84cc23df83102d5e9c9bc36eb4af9f8f31592550
Divya <divs115@gmail.com>
2019-07-12 12:47:36 -0600

    First commit

---
+++ connect.php
@@ -0,0 +1,39 @@
+<?php
+
+$username = filter_input(INPUT_POST, 'username');
+$password = filter_input(INPUT_POST, 'password');
+if(!empty($username)){
+	if(!empty($password)){
+		$host = "hsc-ctsc-development:3306";
+		$dbusername = "dbuser";
+		$dbpassword = "CTSC_2019";
+		$dbname = "PubmedDailyUpdate";
+		
+		//Create connection
+		$conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+		
+		if(mysqli_connect_error()){
+			die('Connect Error ('.mysqli_connect_errno().') '.mysqli_connect_error());
+		}
+		else{
+			$sql = "INSERT INTO account(username, password) values('$username','$password')";
+			if($conn->query($sql)){
+				echo "New record is inserted successfully";
+			}
+			else{
+				echo "Error: ". $sql ."<br>". $conn->error;
+			}
+			$conn->close();
+		}
+	}
+	else{
+		echo "Password should not be empty";
+		die();
+	}
+}
+else{
+	echo "Username should not be empty";
+	die();
+}
+
+?>
---
+++ js/index.js
@@ -0,0 +1,117 @@
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  if(cityName != "Home") document.getElementById(cityName).style.display = "block";
+  else document.getElementById(cityName).style.display = "flex";
+  evt.currentTarget.className += " active";
+}
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+function ExportToTable() {  
+	$("#pmidList").empty();
+	$("#exceltable").empty();
+     var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.xlsx|.xls)$/;  
+     /*Checks whether the file is a valid excel file*/  
+     if (regex.test($("#excelfile").val().toLowerCase())) {  
+         var xlsxflag = false; /*Flag for checking whether excel is .xls format or .xlsx format*/  
+         if ($("#excelfile").val().toLowerCase().indexOf(".xlsx") > 0) {  
+             xlsxflag = true;  
+         }  
+         /*Checks whether the browser supports HTML5*/  
+         if (typeof (FileReader) != "undefined") {  
+             var reader = new FileReader();  
+             reader.onload = function (e) {  
+                 var data = e.target.result;  
+                 /*Converts the excel data in to object*/  
+                 if (xlsxflag) {  
+                     var workbook = XLSX.read(data, { type: 'binary' });  
+                 }  
+                 else {  
+                     var workbook = XLS.read(data, { type: 'binary' });  
+                 }  
+                 /*Gets all the sheetnames of excel in to a variable*/  
+                 var sheet_name_list = workbook.SheetNames;  
+  
+                 var cnt = 0; /*This is used for restricting the script to consider only first sheet of excel*/  
+                 sheet_name_list.forEach(function (y) { /*Iterate through all sheets*/  
+                     /*Convert the cell value to Json*/  
+                     if (xlsxflag) {  
+                         var exceljson = XLSX.utils.sheet_to_json(workbook.Sheets[y]);  
+                     }  
+                     else {  
+                         var exceljson = XLS.utils.sheet_to_row_object_array(workbook.Sheets[y]);  
+                     }  
+                     if (exceljson.length > 0 && cnt == 0) {  
+                         BindTable(exceljson, '#exceltable');  
+                         cnt++;  
+                     }  
+                 });  
+                 $('#exceltable').show();  
+             }  
+             if (xlsxflag) {/*If excel file is .xlsx extension than creates a Array Buffer from excel*/  
+                 reader.readAsArrayBuffer($("#excelfile")[0].files[0]);  
+             }  
+             else {  
+                 reader.readAsBinaryString($("#excelfile")[0].files[0]); 
+             }  
+         }  
+         else {  
+             alert("Sorry! Your browser does not support HTML5!");  
+         }  
+     }  
+     else {  
+         alert("Please upload a valid Excel file!");  
+     }  
+ }  
+function BindTable(jsondata, tableid) {/*Function used to convert the JSON array to Html Table*/  
+     var columns = BindTableHeader(jsondata, tableid); /*Gets all the column headings of Excel*/  
+	 var pmidList = "";
+     for (var i = 0; i < jsondata.length; i++) {  
+         var row$ = $('<tr/>');  
+         for (var colIndex = 0; colIndex < columns.length; colIndex++) {  
+             var cellValue = jsondata[i][columns[colIndex]];  
+			 if(columns[colIndex].toUpperCase() === "PMID".toUpperCase()) pmidList += cellValue+",";
+             if (cellValue == null)  
+                 cellValue = "";  
+             row$.append($('<td/>').html(cellValue));  
+         }  
+         $(tableid).append(row$);  
+     }  
+	 console.log(pmidList.substring(0, pmidList.length-1));
+	 $("#pmidList").css("visibility", "visible");
+	 $("#pmidList").append("<strong>PMIDs List: </strong><br><br>"+pmidList.substring(0, pmidList.length-1));
+ }  
+ function BindTableHeader(jsondata, tableid) {/*Function used to get all column names from JSON and bind the html table header*/  
+     var columnSet = [];  
+     var headerTr$ = $('<tr/>');  
+     for (var i = 0; i < jsondata.length; i++) {  
+         var rowHash = jsondata[i];  
+         for (var key in rowHash) {  
+             if (rowHash.hasOwnProperty(key)) {  
+                 if ($.inArray(key, columnSet) == -1) {/*Adding each unique column names to a variable array*/  
+                     columnSet.push(key);  
+                     headerTr$.append($('<th/>').html(key));  
+                 }  
+             }  
+         }  
+     }  
+     $(tableid).append(headerTr$);  
+     return columnSet;  
+ }
