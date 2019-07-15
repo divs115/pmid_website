536cb68ad900902bae7e36feb0e860b9b54d2017
Divya <divs115@gmail.com>
2019-07-15 13:21:10 -0600

    Created image grid

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 function openCity(evt, cityName) {
+	console.log("HERE");
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -8,8 +9,7 @@ function openCity(evt, cityName) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  if(cityName != "Home") document.getElementById(cityName).style.display = "block";
-  else document.getElementById(cityName).style.display = "flex";
+  document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
 window.onscroll = function() {myFunction()};
